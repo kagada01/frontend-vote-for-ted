da75233644d7267dc9a283224de9bda635c1b463
Dan Kagan <dankagan7@gmail.com>
2019-09-19 12:29:47 -0400

    completed designed for ENTIRE homepage. No functionality on page, besides Nav & Links.

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,21 +1,72 @@
 import React, { Component, Fragment } from 'react';
-import { Divider, Image, Header, Container, Button, Message } from 'semantic-ui-react';
+import { Divider, Image, Header, Container, Button, Icon, Message } from 'semantic-ui-react';
 
 class Footer extends React.Component {
 
 render() {
+    
+  let emailIcon = "email-icon.jpg"
+    
     return(
         
+
      <Fragment>
      
        <Container
        className="main-footer"
-       width={1000}> 
+       > 
         
-            <p>Footer</p>
+        <Button.Group 
+        size="tiny"
+        className="sm-button-group"
+        widths={1}>
+       
+            <Button 
+            size="small"
+            inverted={true}
+            compact={true}
+            icon={true} 
+            color="facebook">
+                <Icon name="facebook" />Facebook
+            </Button>     
+
+            <Button
+            size="small"
+            inverted={true}
+            compact={true}
+            icon={true} 
+            floated="right"
+            color='instagram'>
+                <Icon name='instagram' /> Instagram
+            </Button>
+
+            <Button 
+            size="small"
+            inverted={true}
+            compact={true}
+            icon={true} 
+            color='twitter'>
+                <Icon name='twitter' /> Twitter
+            </Button>   
+
+            <Button 
+            size="small"
+            inverted={true}
+            color='google plus'>
+            Email
+            </Button>
+
+        </Button.Group>   
+           
+        <Header
+        className="created-by-tag"
+        as="h4"
+        >
+        Created by Friends of Ted Steinberg for State Representative 2019
+        </Header>
+
         </Container>
       
-    
     </Fragment>
     )
 }
