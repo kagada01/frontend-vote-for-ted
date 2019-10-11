c149c2944a7688372f028859b736a9b42c2056cb
Dan Kagan <dankagan7@gmail.com>
2019-10-11 13:53:01 -0400

    added Meet Ted page images, created container for Local Roots, added all text (Container component) under main image on Meet Ted page

--- src/containers/MeetTed.js
+++ src/containers/MeetTed.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from "react-router-dom";
-import { Divider, Image, Header, Container, Form, Button, Message } from 'semantic-ui-react';
+import { Divider, List, Image, Header, Container, Form, Button, Message } from 'semantic-ui-react';
 import Footer from "./Footer";
 import JoinTeamTed from "../components/JoinTeamTed"
 
@@ -9,15 +9,40 @@ class MeetTed extends React.Component {
 render() {
     
     let meetTedImage = "meet-ted-main-image.png"
-
+    let localRootsImage = "meet-ted-local-roots-image.png"
+    let demRootsImage = "meet-ted-democratic-activism-image.png"
+    let commService = "meet-ted-community-service-image.png"
 
     return(
     <Fragment>
-
         <Image src={meetTedImage}>
         </Image>
-        <p align="center">Meet Ted</p>
+        <br></br>
+        <br></br>
+        <Container text>
+            <Header 
+            textAlign={"center"}
+            as='h1'>MEET TED</Header>
+                <p>
+                Born and raised in Needham, Ted will be more than just a vote in the State House, he will be a refreshing leader for our district, determined to put words to action.
+                </p>
+        
+                <p>
+                A former Congressional Aide and constituent caseworker, Ted began his community organizing efforts during the 2016 election cycle, coordinating voter registration sign-ups, 
+                leading civic engagement workshops, and volunteering for local Democratic candidates. Earning a degree in Public Policy and a certificate in Public Administration, Ted was 
+                named a 2017 Forbes 30 Under 30 Student Scholar.
+                </p>
+        
+                <p>
+                With extensive experience in local, state, and federal government, Ted is ready to fight as a progressive advocate for the people of Needham, Dover, and Medfield.
+                </p>
+        </Container>
 
+        <div className="local-roots-container">
+        <Image src={localRootsImage}>
+        </Image>
+
+        </div>
 
         <JoinTeamTed />
 
