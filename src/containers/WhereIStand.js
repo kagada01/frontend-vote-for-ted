638de2448772d187296af99879651c4cc00297df
Dan Kagan <dankagan7@gmail.com>
2019-10-16 08:04:31 -0400

    completed 8/12 Where I Stand policy sections - NOT refactored

--- src/containers/WhereIStand.js
+++ src/containers/WhereIStand.js
@@ -234,6 +234,202 @@ render() {
         <br></br>
 
 
+        <Divider></Divider>
+
+                    {/* CRIMINAL JUSTICE REFORM SECTION */}
+
+        <br></br>
+        <br></br>
+
+        <Container text className="where-I-stand-criminal-justice-ref-container">
+            <Header as="h2">CRIMINAL JUSTICE REFORM</Header>
+            
+            <p>
+            Our government must make changes to truly fulfill 
+            the promise of justice for all. The racial and socio-economic 
+            disparities create an inequitable society that perpetuates cyclical hardship. 
+            With comprehensive criminal justice reform, we can level the playing 
+            field and focus on rehabilitation rather than incarceration. 
+            Locking up non-threatening individuals merely shifts the burdens 
+            onto local communities.
+            </p>
+        
+            <List bulleted={true} className="where-I-stand-criminal-justice-ref-bl">
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation to remove mandatory minimums for non-violent, non-threatening offenders
+                </List.Item>
+                
+                <List.Item> 
+                    <b>Action Item:</b> Sponsor legislation promoting alternatives to incarceration
+                </List.Item> 
+                    
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation to reinvest prison population savings into job training and employment programs for reformed convicts
+                </List.Item>    
+                    
+                <List.Item>
+                    <b>Action Item:</b> Enhance and protect the Safe Communities Act
+                </List.Item>
+
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation prohibiting the militarization of police resources
+                </List.Item>
+                
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation encouraging communities to direct funds towards Juvenile Diversion Programs
+                </List.Item>
+            </List>
+
+        </Container>
+
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <Divider></Divider>
+
+                    {/* HEALTHCARE SECTION */}
+
+        <br></br>
+        <br></br>
+
+        <Container text className="where-I-stand-healthcare-container">
+            <Header as="h2">HEALTHCARE</Header>
+            
+            <p>
+            It is time to implement healthcare for all. 
+            The inability to access affordable healthcare continues 
+            to plague American families, where even in Massachusetts 
+            premiums hikes surpass income growth. When every American is covered, 
+            total healthcare costs plummet. As the opioid crisis continues to 
+            devastate our communities, and Congress works to gut the 
+            Affordable Care Act, the State House must step up.
+            </p>
+        
+            <List bulleted={true} className="where-I-stand-healthcare-bl">
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation to provide universal healthcare in Massachusetts
+                </List.Item>
+                
+            </List>
+
+        </Container>
+
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+
+        <Divider></Divider>
+
+                    {/* GOVERNMENT ACCOUNTABILITY & TRANSPARENCY SECTION */}
+
+        <br></br>
+        <br></br>
+
+        <Container text className="where-I-stand-govt-accountability-container">
+            <Header as="h2">GOVERNMENT ACCOUNTABILITY & TRANSPARENCY</Header>
+            
+            <p>
+            Home to some of the worst public records laws in the country, 
+            with low rates of compliance for public information requests, 
+            Massachusetts residents deserve better than the status quo. 
+            Our representatives rely on old-school political tactics to avoid 
+            taking difficult positions and confronting uncomfortable issues. 
+            Participating in unrecorded votes and succumbing to party 
+            leadership’s wishes leads to a legislature that sacrifices 
+            key policy priorities to maintain perceived unanimity. 
+            This is not how legislative bodies should function. We need more 
+            responsive, more transparent, and more accountable representation.
+            </p>
+        
+            <List bulleted={true} className="where-I-stand-govt-accountability-bl">
+                <List.Item>
+                    <b>Action Item:</b> Maintain up-to-date voting log with explanations
+                </List.Item>
+                
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation making recorded votes the default
+                </List.Item>
+
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation instituting term limits
+                </List.Item>
+
+                <List.Item>
+                    <b>Action Item:</b> Sponsor legislation instituting automatic voter registration
+                </List.Item>
+
+                <List.Item>
+                    <b>Action Item:</b> Sponsor the We The People Act to take dark money out of politics
+                </List.Item>
+
+            </List>
+
+        </Container>
+
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+
+        <Divider></Divider>
+
+{/* SEXUAL ASSAULT & DOMESTIC VIOLENCE SECTION */}
+
+<br></br>
+<br></br>
+
+<Container text className="where-I-stand-sexual-assault-domestic-violence-container">
+<Header as="h2">SEXUAL ASSAULT & DOMESTIC VIOLENCE</Header>
+
+<p>
+As sexual assault and domestic violence continue to plague society, 
+it is clear our Commonwealth must do more to combat this epidemic. 
+One in five women on college campuses experience sexual assault. 
+We need more proactive solutions. Mandatory consent education courses 
+will teach our future leaders the importance of healthy relationships, 
+and provide techniques for bystander intervention. We need to update 
+the state institutions dedicated to sexual assault and domestic violence, 
+ensuring accessible and comprehensive support and resources. 
+There are too many “me too” stories to maintain the status quo of inaction.
+</p>
+
+<List bulleted={true} className="where-I-stand-sexual-assault-domestic-violence-bl">
+<List.Item>
+<b>Action Item:</b> Develop Consent Education for all high school seniors in state aided institutions
+</List.Item>
+
+<List.Item>
+<b>Action Item:</b> Sponsor legislation to create sexual assault kit tracking system
+</List.Item>
+
+<List.Item>
+<b>Action Item:</b> Require sexual assault and harassment training for all state employees
+</List.Item>
+
+<List.Item>
+<b>Action Item:</b> Require all higher education institutions to report sexual assault statistics to state agencies
+</List.Item>
+
+<List.Item>
+<b>Action Item:</b> End the use of non-disclosure agreements for State House staff that file complaints about sexual assault and/or harrassment
+</List.Item>
+
+</List>
+
+</Container>
+
+
+<br></br>
+<br></br>
+<br></br>
+
+
+
         <JoinTeamTed/>
         <Footer/>
 
